8a802f01b33a29be06b0ee6e6683b4d2b26812a4
Vladislav Piontak <foltshayt@gmail.com>
2025-04-14 03:19:55 +0300

    Implemented - Database

--- app/src/main/java/com/vlad/newsapp/data/network/Network.kt
+++ app/src/main/java/com/vlad/newsapp/data/network/Network.kt
@@ -1,6 +1,7 @@
 package com.vlad.newsapp.data.network
 
 import com.vlad.newsapp.data.network.api_service.NewsService
+import com.vlad.newsapp.utils.constants.API_KEY
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
@@ -38,14 +39,12 @@ private fun getApiService(retrofit: Retrofit): NewsService {
 
 class MainInterceptor: Interceptor{
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request().newBuilder().addHeader("X-Api-Key", TOKEN_2)
+        val request = chain.request().newBuilder().addHeader("X-Api-Key", API_KEY)
             .build()
         val response = chain.proceed(request)
         return response
     }
 }
 
-private const val TOKEN_1 = "3192a1bdf3954c7780708cf1611a4567"
-private const val TOKEN_2 = "885eaadcecc24ac3a357d792cf986fbb"
-private const val TOKEN_3 ="a109bc03fb0e4d6cbf00112a914d027b"
-private const val TOKEN_4 ="b9a1d75ec42c462398e05106b1f8ac76"
+
+
--- app/src/main/java/com/vlad/newsapp/utils/constants/Constants.kt
+++ app/src/main/java/com/vlad/newsapp/utils/constants/Constants.kt
@@ -7,3 +7,5 @@ const val DEFAULT_NONE = "entertainment"
 const val DATABASE_NAME = "news_database"
 
 const val STATUS_OK = "ok"
+
+const val API_KEY = "885eaadcecc24ac3a357d792cf986fbb"
